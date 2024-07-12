139021aa984547c25502086a5286f1fcd9440aa4
Parth-09 <parthtawde2000@gmail.com>
2024-07-12 10:39:39 -0500

    More commits

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,18 +1,16 @@
 import React from "react";
-import { QRCodeCanvas } from 'qrcode.react';
-const eventLink = 'http://localhost:3000/';
+import { QRCodeCanvas } from "qrcode.react";
+const eventLink = "https://stoop-sale-pi.vercel.app/";
 const Details = () => (
-  
   <section id="details" className="details-container">
-      <p>
-        Join us for a charming stoop sale in the heart of Brooklyn, NY. 
-        We'll have a delightful array of items including clothes, home goods, shoes, and
-        unique tchotchkes.
-        You'll find us at the corner of 2nd Pl. and Court St.
-      </p>
-      <p>Date: July 15th, 2024</p>
-      <p>Time: 10:00 AM - 4:00 PM</p>
-      <QRCodeCanvas value={eventLink} size={128} />
+    <p>
+      Join us for a charming stoop sale in the heart of Brooklyn, NY. We'll have
+      a delightful array of items including clothes, home goods, shoes, and
+      unique tchotchkes. You'll find us at the corner of 2nd Pl. and Court St.
+    </p>
+    <p>Date: July 15th, 2024 <br/>
+    Time: 10:00 AM - 4:00 PM</p>
+    <QRCodeCanvas value={eventLink} size={128} />
   </section>
 );
 
